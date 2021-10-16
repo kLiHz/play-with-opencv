97bcb20569fa54822de8fede8079ad8af55495e9
henry_23 <henry.maoht@outlook.com>
2021-10-16 12:56:06 +0800

    fix(hybrid): several improvements

--- hybrid/main.cpp
+++ hybrid/main.cpp
@@ -39,8 +39,38 @@ auto buildLaplacianPyramid(cv::Mat const & a, int maxLevel) {
 
 #include <fmt/core.h>
 
+using MatVector = std::vector<cv::Mat>;
+
+std::tuple<cv::Mat, MatVector, MatVector> 
+getLinearHybridImage(cv::Mat const & img1, double a, cv::Mat const & img2, double b, int maxLayerLevel) {
+    auto L1 = buildLaplacianPyramid(img1, maxLayerLevel);
+    auto L2 = buildLaplacianPyramid(img2, maxLayerLevel);
+
+    std::vector<cv::Mat> L3;
+
+    for (auto i = 0; i <= maxLayerLevel; ++i) {
+        L3.push_back(
+            (a * L1[i]) + (b * L2[i])
+        );
+    }
+
+    // reconstruct
+    auto result = L3[maxLayerLevel];
+    for (auto i = maxLayerLevel - 1; i >= 0; --i) {
+        result = pyrUp(result) + L3[i];
+    }
+
+    return {result, L1, L2};
+}
+
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
 int main(int argc, char *argv[])
 {
+    _putenv("QT_AUTO_SCREEN_SCALE_FACTOR=1");
+
     auto keys = 
         "{help h ?  |       | Print this message               }"
         "{@image1   |<none> | First image's path               }"
@@ -67,9 +97,17 @@ int main(int argc, char *argv[])
     auto imgPath3 = parser.get<std::string>("@image3");
 
     if (!parser.check()) {
+        parser.printMessage();
         parser.printErrors();
         return 0;
     }
+
+    std::cout 
+        << std::boolalpha
+        << "Max layer number: " << n << "\n"
+        << "Total pyramid layers: " << n + 1 << "\n"
+        << "Write out layers of pyramids: " << verbose << "\n"
+        << "Use imshow to visualize pyramids: " << visual << "\n";
 
     auto img1 = cv::imread(imgPath1);
     auto img2 = cv::imread(imgPath2);
@@ -86,51 +124,68 @@ int main(int argc, char *argv[])
 
     std::cout << "Image loaded successfully." << std::endl;
 
-    cv::cvtColor(img1, img1, cv::COLOR_BGR2GRAY);
-    cv::cvtColor(img2, img2, cv::COLOR_BGR2GRAY);
+    cv::Mat result;
 
-    auto L1 = buildLaplacianPyramid(img1, n);
-    auto L2 = buildLaplacianPyramid(img2, n);
-
-    // merging
-
-    std::vector<cv::Mat> L3;
-
-    for (auto i = 0; i <= n; ++i) {
-        L3.push_back(
-            (0.5 * L1[i]) + (0.5 * L2[i])
-        );
-    }
-    
-    // reconstruct
-
-    auto result = L3[n];
-
-    for (auto i = n - 1; i >= 0; --i) {
-        result = pyrUp(result) + L3[i];
-    }
-
-    if (visual) {
+    auto viewLapPyr = [&](MatVector const & L1, MatVector const & L2, std::string winNamePrefix = "") {
         for (int i = 0; i <= n; ++i) {
-            auto winName1 = fmt::format("{}: L[{}]", imgPath1, i);
-            auto winName2 = fmt::format("{}: L[{}]", imgPath2, i);
+            auto winName1 = fmt::format("{} {}: L[{}]", winNamePrefix, imgPath1, i);
+            auto winName2 = fmt::format("{} {}: L[{}]", winNamePrefix, imgPath2, i);
             cv::namedWindow(winName1, cv::WINDOW_NORMAL);
             cv::namedWindow(winName2, cv::WINDOW_NORMAL);
             cv::imshow(winName1, L1[i]);
             cv::imshow(winName2, L2[i]);
         }
+        cv::waitKey(0);
+        cv::destroyAllWindows();
+    };
+
+    auto wirteOutLapPyr = [&](MatVector const & L1, MatVector const & L2) {
+        for (int i = 0; i <= n; ++i) {
+            fs::create_directory("./hybrid-out/");
+            fs::create_directory("./hybrid-out/img1/");
+            fs::create_directory("./hybrid-out/img2/");
+            cv::imwrite(fmt::format("./hybrid-out/img1/L_{}.png", i), L1[i]);
+            cv::imwrite(fmt::format("./hybrid-out/img2/L_{}.png", i), L2[i]);
+        }
+    };
+
+    if (img1.channels() != img2.channels()) {
+        if (img1.channels() != 1) {
+            cv::cvtColor(img1, img1, cv::COLOR_BGR2GRAY);
+            cv::cvtColor(img2, img2, cv::COLOR_BGR2GRAY);
+        }
+        auto [result, L1, L2] = getLinearHybridImage(img1, 0.5, img2, 0.5, n);
+        if (visual) viewLapPyr(L1, L2);
+        if (verbose) wirteOutLapPyr(L1, L2);
+    } else {
+        std::vector<cv::Mat> split1, split2, split3;
+        cv::split(img1, split1);
+        cv::split(img2, split2);
+        split3.resize(img1.channels());
+        for (int i = 0; i < split1.size(); ++i) {
+            auto [a, L1, L2] = getLinearHybridImage(split1[i], 0.5, split2[i], 0.5, n);
+            split3[i] = a;
+            if (visual) viewLapPyr(L1, L2, fmt::format("Channel {}:", i));
+        }
+        cv::merge(split3, result);
+        
+        cv::merge(split1, img1);
+        cv::merge(split2, img2);
+
+        // Got L1, L2 from GRAY image; compromised solution
+        cv::cvtColor(img1, img1, cv::COLOR_BGR2GRAY);
+        cv::cvtColor(img2, img2, cv::COLOR_BGR2GRAY);
+        auto [r, L1, L2] = getLinearHybridImage(img1, 0.5, img2, 0.5, n);
+        if (verbose) wirteOutLapPyr(L1, L2);
+    }
+
+    if (visual) {
         cv::namedWindow("result", cv::WINDOW_NORMAL);
         cv::imshow("result", result);
         cv::waitKey(0);
+        cv::destroyAllWindows();
     }
 
-    if (verbose) {
-        for (int i = 0; i <= n; ++i) {
-            auto pattern = "./hybrid-out/{}/L_{}.png";
-            cv::imwrite(fmt::format(pattern, "img1", i), L1[i]);
-            cv::imwrite(fmt::format(pattern, "img2", i), L2[i]);
-        }
-    }
     cv::imwrite(imgPath3, result);
     return 0;
 }
